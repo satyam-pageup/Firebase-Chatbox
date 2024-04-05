df1e670e98051dc1906d6cf877f7da78d2e9a7e1
om-pageup <obhandari@pageupsoft.com>
2024-04-05 15:03:47 +0530

    pull

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { getMessaging, provideMessaging } from '@angular/fire/messaging';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp({"projectId":"firebase-chat-angular","appId":"1:582344153186:web:102bd305ce63683c81593b","databaseURL":"https://firebase-chat-angular-default-rtdb.firebaseio.com","storageBucket":"firebase-chat-angular.appspot.com","apiKey":"AIzaSyBDVUswYSUrKDZuuNS_1_KOQvGStpQCTTQ","authDomain":"fir-chat-angular-234df.firebaseapp.com","messagingSenderId":"582344153186"})),
     provideFirestore(() => getFirestore()),
